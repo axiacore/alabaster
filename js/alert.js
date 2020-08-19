ed1aadc96d6194b7cfc75df276b5a196bbc196de
Camilo Nova <camilo.nova@axiacore.com>
2020-08-19 09:11:06 -0500

    Added default options

--- js/alert.js
+++ js/alert.js
@@ -1,38 +1,37 @@
 import $ from 'jquery';
+import 'featherlight';
 
 import ax3NoScroll from './no-scroll.js';
 
-$.fn.extend({
-    showAlert(html, options) {
-        $.featherlight({
-            html: html,
-            closeOnEsc: false,
-            closeTrigger: null,
-            namespace: 'modal',
-            variant: 'modal_alert',
-            afterContent() {
-                let that = this;
-                ax3NoScroll(true);
+export default function showAlert(html, options = {}) {
+    $.featherlight({
+        html: html,
+        closeOnEsc: false,
+        closeTrigger: null,
+        namespace: 'modal',
+        variant: 'modal_alert',
+        afterContent() {
+            let that = this;
+            ax3NoScroll(true);
 
-                if (options.loadedCallback !== undefined) { options.loadedCallback(that); }
+            that.$instance.find('.js-alert-btn-close').on('click', () => {
+                that.close();
+            });
 
-                that.$instance.find('.js-alert-btn-close').on('click', () => {
-                    that.close();
-                });
+            if (options.loadedCallback !== undefined) { options.loadedCallback(that); }
 
-                that.$instance.find('.js-alert-btn-cancel').on('click', () => {
-                    if (options.cancelCallback !== undefined) { options.cancelCallback(that); }
-                    else { that.close(); }
-                });
+            that.$instance.find('.js-alert-btn-cancel').on('click', () => {
+                if (options.cancelCallback !== undefined) { options.cancelCallback(that); }
+                else { that.close(); }
+            });
 
-                that.$instance.find('.js-alert-btn-done').on('click', () => {
-                    if (options.doneCallback !== undefined) { options.doneCallback(that); }
-                    else { that.close(); }
-                });
-            },
-            afterClose() {
-                ax3NoScroll(false);
-            }
-        });
-    }
-});
+            that.$instance.find('.js-alert-btn-done').on('click', () => {
+                if (options.doneCallback !== undefined) { options.doneCallback(that); }
+                else { that.close(); }
+            });
+        },
+        afterClose() {
+            ax3NoScroll(false);
+        }
+    });
+};
