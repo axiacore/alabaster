48009720b266d35b86960bf7d05a4fce0dc72311
Santiago Feijoo <santiago.feijoo@axiacore.com>
2021-12-10 18:35:13 -0500

    Improve no scroll functionality

--- js/no-scroll.js
+++ js/no-scroll.js
@@ -1,16 +1,18 @@
 function ax3NoScroll(fixed) {
     let classScroll = 'no-scroll';
     let body = document.body;
+    let html = document.getElementsByTagName('html')[0];
+
     if (fixed && body.dataset.modalScrollTop === undefined) {
-        let scrollY = window.scrollY;
+        const scrollY = window.scrollY;
+        html.classList.add(classScroll);
         body.style.top = `${-scrollY}px`;
         body.dataset.modalScrollTop = scrollY;
-        body.classList.add(classScroll);
     } else if (!fixed) {
+        html.classList.remove(classScroll);
         window.scrollTo(0, parseInt(body.dataset.modalScrollTop || '0'));
         body.style.top = '';
         body.removeAttribute('data-modal-scroll-top');
-        body.classList.remove(classScroll);
     }
 }
 
